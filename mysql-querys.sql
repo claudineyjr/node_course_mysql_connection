a7777fee7bf1ec3814e02f45683b2850815d68e8
Claudiney Junior <claudiney.alves@fatec.sp.gov.br>
2018-07-23 23:47:00 -0300

    Nome da tabela modificado

--- mysql-querys.sql
+++ mysql-querys.sql
@@ -2,16 +2,16 @@ CREATE DATABASE `curso`;
 
 USE `curso`;
 
-CREATE TABLE `curso`.`alunos`(
+CREATE TABLE `curso`.`aluno`(
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(250) NOT NULL,
   `birthday` DATE NOT NULL
 );
 
-INSERT INTO `curso`.`alunos`
+INSERT INTO `curso`.`aluno`
 	(`name`, `birthday`)
 VALUES
 	('Claudiney Junior', '1988-10-24'),
   ('Roberto Mandolesi', '1990-01-01');
 
-SELECT * FROM `alunos`;
+SELECT * FROM `aluno`;
